e30c9e0f2109a015716e995a1a50c9f797761f0e
ChoiHyeongu <a11dsd123@gmail.com>
2019-06-06 23:57:50 +0900

    PerfectLoopMediaPlayer

--- app/src/main/java/songpatechnicalhighschool/motivation/geuleuneuda/InduceActivity.java
+++ app/src/main/java/songpatechnicalhighschool/motivation/geuleuneuda/InduceActivity.java
@@ -2,28 +2,41 @@ package songpatechnicalhighschool.motivation.geuleuneuda;
 
 import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
+import android.media.PlaybackParams;
 import android.media.SoundPool;
+import android.media.ToneGenerator;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.concurrent.Delayed;
 
 public class InduceActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
-    public int delayMillis = 1000;
+    public float speed = 6f;
+    final float changeSize = 0.25f;
     private int beep = R.raw.beep;
+    private boolean speedChanged = true;
+    private PerfectLoopMediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_induce);
 
-
+        //mediaPlayer = MediaPlayer.create(this, beep);
+        //mediaPlayer.setVolume(1f, 1f);
+        //mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(0.25f));
+        //mediaPlayer.setLooping(true);
 
         playSound();
 
@@ -37,16 +50,38 @@ public class InduceActivity extends AppCompatActivity {
 
     }
 
-    public void playSound() {
+    void playSound() {
+        Handler delay = new Handler();
 
+        player = PerfectLoopMediaPlayer.create(this, beep);
+        player.start();
+
+        PlaybackParams playbackParams = new PlaybackParams();
+
+        playbackParams.setPitch(1f);
+        playbackParams.setSpeed(0.25f);
+        player.changeSpeed(playbackParams);
+        Toast.makeText(this, "1f", Toast.LENGTH_SHORT).show();
+        delay.postDelayed(()->playbackParams.setPitch(2f), 3000);
+        playbackParams.setSpeed(2f);
+        player.changeSpeed(playbackParams);
+        Toast.makeText(this, "2f", Toast.LENGTH_SHORT).show();
+        delay.postDelayed(()->playbackParams.setPitch(4f), 3000);
+        playbackParams.setSpeed(4f);
+        player.changeSpeed(playbackParams);
+        Toast.makeText(this, "4f", Toast.LENGTH_SHORT).show();
+    }
+
+    private void changeplayerSpeed(float speed) {
     }
 
     public void stopSound() {
-        mediaPlayer.stop();
+        player.stop();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        player.release();
     }
 }
